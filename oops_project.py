0a802c716d84311e33ef5358ebdebac234275180
Aniket.Ugale <aniket.ugale@hatchfast.dev>
2025-04-07 22:16:02 +0530

    Signup and Signin Functions added

--- oops_project.py
+++ oops_project.py
@@ -16,16 +16,49 @@ class chatbook:
                            """)
         
         if user_input == "1":
-            pass
+            self.signup()
+
         elif user_input == "2":
-            pass
+            self.signin()
+
         elif user_input == "3":
             pass
+
         elif user_input == "4":
             pass
+
         elif user_input == "5":
             pass
+        
         else:
             exit()
+        
+
+    def signup(self):
+        email  = input("Enter Your Email:")
+        password = input("Enter Your Password:")
+
+        self.username = email
+        self.password = password
+        print("You have successfully signed up!")
+        print("\n")
+
+        self.menu()
+
+    def signin(self):
+        if self.username =="" and self.password =="":
+            print("Please sign up first")
+        else:
+            u_name = input("Enter Your Email:")
+            pwd  = input("Enter Your Password:")
+
+            if u_name == self.username and pwd == self.password:
+                print("You have successfully signed in!")
+                self.logged_in = True
+            else:
+                print("Invalid credentials, please try again")
+            print("\n")
+        self.menu()
+
 
 object = chatbook()
